d390be2baf32a572e5448888ae05c53c10dcce0b
Zergio Ruvalcaba. Torres <zruva002@sledge.cs.ucr.edu>
2022-04-05 16:00:22 -0700

    Before starting lab1

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "user.h"
+
+int
+main (int argc, char **argv)
+{
+    printf(1, "hello cs153\n"); // fd=1 means standard output.
+    printf(1, "hi there breakpoint\n");
+    return 0;
+    //change
+    //exit();
+}
--- sysproc.c
+++ sysproc.c
@@ -16,6 +16,7 @@ sys_fork(void)
 int
 sys_exit(void)
 {
+  //int status;
   exit();
   return 0;  // not reached
 }
